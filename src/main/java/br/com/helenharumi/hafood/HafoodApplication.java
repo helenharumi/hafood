8a33ba8521a19cb49750df37d93741c7c1151733
Helen Harumi <helen.harumi1@gmail.com>
2021-03-20 20:28:17 -0300

    commit inicial

---
+++ src/main/java/br/com/helenharumi/hafood/HafoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.helenharumi.hafood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HafoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HafoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/helenharumi/hafood/HafoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.helenharumi.hafood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HafoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
